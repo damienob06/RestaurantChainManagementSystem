44275a6bcaafbe22d3a7ca3024dcf6fc607176ab
MarkTGeary <Marktgeary@gmail.com>
2025-12-01 14:34:11 +0000

    adding stuff

---
+++ src/main/java/gitIgnore/demo/GitIgnoreApplication.java
@@ -0,0 +1,13 @@
+package gitIgnore.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitIgnoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitIgnoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gitIgnore/demo/GitIgnoreApplicationTests.java
@@ -0,0 +1,13 @@
+package gitIgnore.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitIgnoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
